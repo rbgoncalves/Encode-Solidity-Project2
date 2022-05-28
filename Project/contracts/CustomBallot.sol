ae00212fa8360ad8977148aebafcc46fa5183b57
Roman Svarichevsky <n.e.w_w.a.v.e@mail.ru>
2022-05-28 12:44:39 +0300

    Update
    Comment Visibility

--- Project/contracts/CustomBallot.sol
+++ Project/contracts/CustomBallot.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 interface IERC20Votes {
     function getPastVotes(address, uint256) external view returns (uint256);
+//don't work with internal view
 }
 
 contract CustomBallot {
